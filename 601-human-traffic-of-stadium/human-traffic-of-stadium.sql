bc1b0662a74dc97e66d595b155aad8e26836a6fb
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-08-04 13:29:59 +0530

    Time: 340 ms (86.51%) | Memory: 0B (100.00%) - LeetSync

--- 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
+++ 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
@@ -1,19 +1,19 @@
 # Write your MySQL query statement below
-WIth TABLE1 AS 
-    (SELECT A.id as AID,A.visit_date AS AV,A.people AS AP,
-            B.id as BID,B.visit_date AS BV,B.people AS BP,
-            C.id AS CID,C.visit_date AS CV,C.people AS CP
-    FROM Stadium AS A
-    JOIN Stadium AS B
-        ON A.id + 1= B.id AND B.people>= 100 AND A.people >= 100 
-    JOIN Stadium AS C
-        ON B.id + 1= C.id AND C.people>=100 AND B.people >= 100)
-SELECT AID AS id,AV AS visit_date,AP AS people
-FROM Table1
-UNION
-SELECT BID AS id,BV AS visit_date,BP AS people
-FROM Table1
-UNION 
-SELECT CID AS id,CV AS visit_date,CP AS people
-FROM Table1
-ORDER BY id;
+WITH cte AS(
+    select *
+    FROM Stadium
+    WHERE people >= 100
+),
+filtered AS(
+    select *,id-ROW_NUMBER() OVER(ORDER BY id) AS grp
+    FROM cte
+),
+grouped AS(
+    select grp
+    FROM filtered
+    GROUP BY grp
+    HAVING COUNT(*) >= 3
+)
+SELECT id,visit_date,people
+FROM filtered
+WHERE grp in (SELECT grp FROM grouped);
