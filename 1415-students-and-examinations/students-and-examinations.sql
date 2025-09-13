dec11ff610b2a2058f0af4b139ee4ebd95e7caf9
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-09-13 18:44:08 +0530

    Time: 868 ms (79.43%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,15 +1,16 @@
 # Write your MySQL query statement below
-SELECT A.student_id student_id,
-    A.student_name student_name,
-    B.subject_name subject_name,
-    COUNT(
-        C.subject_name
-    )  attended_exams
-FROM Students AS A
-CROSS JOIN Subjects AS B
-LEFT JOIN Examinations AS C
-    ON A.student_id = C.student_id
-    AND B.subject_name = C.subject_name
+ SELECt A.student_id,
+        A.student_name,
+        B.subject_name,
+        SUM(
+            CASE
+                WHEN C.subject_name IS NOT NULL THEN 1
+                ELSE 0
+            END
+        ) AS attended_exams
+FROM Students A
+CROSS JOIN Subjects B
+LEFT JOIN Examinations C
+    ON A.student_id = C.student_id And B.subject_name = C.subject_name
 GROUP BY 1,3
-ORDER BY A.student_id,B.subject_name;
-    
+ORDER BY 1,2,3;
