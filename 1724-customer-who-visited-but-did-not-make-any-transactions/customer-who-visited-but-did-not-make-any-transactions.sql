52110d2171b05a54f92b797a6c73de0de4300fb2
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-09-13 18:19:13 +0530

    Time: 1203 ms (89.56%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,22 +1,8 @@
 # Write your MySQL query statement below
-WITH Table1 AS(
-    SELECT 
-        
-        visit_id,
-        MAX(amount) AS amount
-
-    FROM Transactions
-    GROUP BY visit_id
-)
-
-
-
-SELECT 
-    A.customer_id,
-    COUNT(*) AS count_no_trans
+select a.customer_id,
+   COUNT(*) AS count_no_trans
 FROM Visits A
-LEFT JOIN Table1 B
+LEFT JOIN Transactions B
     ON A.visit_id = B.visit_id
-WHERE B.amount is NULL
-GROUP BY customer_id
-;
+WHERE B.visit_id is NULL
+GROUP BY 1;
